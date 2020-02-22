830c09e33057716ef487f80b05b4624d81ad71a9
Wadiyaali <wadiamubarakali@gmail.com>
2020-02-22 13:18:18 +0500

    First commit

---
+++ ProjectUIweb/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ProjectUIweb.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ProjectUIweb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectUIweb.Startup))]
+namespace ProjectUIweb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
